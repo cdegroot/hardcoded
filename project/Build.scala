fa60b11530ca39d72501a46398b12f552d304d9c
Cees de Groot <cees@pagerduty.com>
2015-08-23 12:25:47 -0400

    Build with 2.10 as well

--- project/Build.scala
+++ project/Build.scala
@@ -2,13 +2,13 @@ import sbt._
 import sbt.Keys._
 
 object HardcodedBuild extends Build {
-  val buildScalaVersion = "2.11.7"
+  //val buildScalaVersion = "2.11.7"
+  val buildScalaVersion = "2.10.5"
 
   lazy val root = Project(id = "hardcoded", base = file("."))
     .configs(IntegrationTest)
     .settings(
       scalaVersion := buildScalaVersion,
-      crossScalaVersions := Seq("2.10.5", buildScalaVersion),
       scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-Xfatal-warnings"),
       libraryDependencies ++= Seq(
         "org.scala-lang" % "scala-reflect" % buildScalaVersion,
