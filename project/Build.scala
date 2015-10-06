54b0721afc24e0ece57745ac189e00f078c70c83
Cees de Groot <cees@pagerduty.com>
2015-10-05 21:26:07 -0400

    Move to Scala 2.11
    Now that https://github.com/jitpack/jitpack.io/issues/178 has been
    fixed, scala versions can be specified on-demand. So we can move
    default development to 2.11, have Travis check 2.10 too, and tell
    people that JitPack will take care of the rest :)

--- project/Build.scala
+++ project/Build.scala
@@ -2,16 +2,15 @@ import sbt._
 import sbt.Keys._
 
 object HardcodedBuild extends Build {
-  //val buildScalaVersion = "2.11.7"
-  val buildScalaVersion = "2.10.5"
 
   lazy val root = Project(id = "hardcoded", base = file("."))
     .configs(IntegrationTest)
     .settings(
-      scalaVersion := buildScalaVersion,
+      scalaVersion := "2.11.7",
+      crossScalaVersions := Seq("2.10.6", "2.11.7"),
       scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature", "-Xfatal-warnings"),
       libraryDependencies ++= Seq(
-        "org.scala-lang" % "scala-reflect" % buildScalaVersion,
+        "org.scala-lang" % "scala-reflect" % scalaVersion.value,
         "org.scalatest" %% "scalatest" % "2.2.4" % "it,test",
         "org.scalamock" %% "scalamock-scalatest-support" % "3.2" % "it,test"))
 
